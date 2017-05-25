38d240d369d3c291212306c4fab79f1ad3b545b6
NikitaOMGTU55 <skipper55rus@mail.ru>
2017-05-25 23:31:14 +0600

    add entities in project

---
+++ Enrollment/Infrastructure.Orm/Entities/Achievements.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace Infrastructure.Orm.Entities
+{
+    public class Achievements
+    {
+        public DataTable AchievementSelect()
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=pmfi.db; Version=3;");
+            conn.Open();
+            SQLiteCommand selectCommand = conn.CreateCommand();
+            string selectText = "select A.AchievementName"
+            + " from Achievements as A";
+            selectCommand.CommandText = selectText;
+            DataTable achievementsDT = new DataTable();
+            achievementsDT.Reset();
+            SQLiteDataAdapter selectDA = new SQLiteDataAdapter(selectCommand);
+            selectDA.Fill(achievementsDT);
+            conn.Close();
+            return achievementsDT;
+        }
+    }
+}
---
+++ Enrollment/Infrastructure.Orm/Entities/Specialty.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace Infrastructure.Orm.Entities
+{
+    public class Specialty
+    {
+        public DataTable SpecialtySelect(string specName)
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=pmfi.db; Version=3;");
+            conn.Open();
+            SQLiteCommand selectCommand = conn.CreateCommand();
+            string selectText = "Select Spec.SpecialtyName, Spec.SpecialtyCount"
+                + " from Specialty as Spec"
+                + " where Spec.SpecialtyName=@Specialty";
+            selectCommand.CommandText = selectText;
+            selectCommand.Parameters.AddWithValue("@Specialty", specName);
+            DataTable specialtyDT = new DataTable();
+            specialtyDT.Reset();
+            SQLiteDataAdapter selectDA = new SQLiteDataAdapter(selectCommand);
+            selectDA.Fill(specialtyDT);
+            conn.Close();
+            return specialtyDT;
+        }
+
+        public DataTable SpecialtySelect()
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=pmfi.db; Version=3;");
+            conn.Open();
+            SQLiteCommand selectCommand = conn.CreateCommand();
+            string selectText = "Select Spec.SpecialtyID as ID, Spec.SpecialtyName as Name, Spec.SpecialtyCount as Count"
+                + " from Specialty as Spec";
+            selectCommand.CommandText = selectText;
+            DataTable specialtyDT = new DataTable();
+            specialtyDT.Reset();
+            SQLiteDataAdapter selectDA = new SQLiteDataAdapter(selectCommand);
+            selectDA.Fill(specialtyDT);
+            conn.Close();
+            return specialtyDT;
+        }
+    }
+}
