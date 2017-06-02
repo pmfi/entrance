cb552aec9a34eb394330bb52076ff3bcca25202c
NikitaOMGTU55 <skipper55rus@mail.ru>
2017-06-02 16:07:12 +0600

    Added sorting by score

--- Enrollment/Infrastructure.WebApi/FakeReadingServices/Impl/FakeDataService.cs
+++ Enrollment/Infrastructure.WebApi/FakeReadingServices/Impl/FakeDataService.cs
@@ -7,79 +7,84 @@ using System;
 
 namespace Infrastructure.WebApi.FakeReadingServices.Impl
 {
-  public class FakeDataService : IFakeDataService
-  {
+    public class FakeDataService : IFakeDataService
+    {
         Students std = new Students();
-        Specialty sp = new Specialty();
-        
-    public IEnumerable<StudentDto> GetStudentsByFilter(StudentFilterDto filter)
-    {
-      return fakeStudents.Where(
-        s => (string.IsNullOrWhiteSpace(filter.Surname) || s.Surname.ToLower().StartsWith(filter.Surname.ToLower())) &&
-             (!filter.SpecialityId.HasValue || s.Priorities.Any(p => p.SpecialityId == filter.SpecialityId)));
-    }
-
-    public IEnumerable<SpecialityDto> GetAllSpecialities()
-    {
-      return fakeSpecialities;
-    }
-
-    public FakeDataService()
-    {
-    fakeStudents = new List<StudentDto>(){};
-    DataTable studDT = std.StudentSelect(1);
-    foreach(DataRow dr in studDT.Rows)
-    {    
-        DataTable studSpec = std.StudentSelect(1, dr["FirstName"].ToString(), dr["Surname"].ToString(), dr["MiddleName"].ToString());
-        fakeStudents.Add(new StudentDto()
+        Specialty spec = new Specialty();
+        public IEnumerable<StudentDto> GetStudentsByFilter(StudentFilterDto filter)
         {
-            Surname = studSpec.Rows[0]["Surname"].ToString(),
-            FirstName = studSpec.Rows[0]["FirstName"].ToString(),
-            MiddleName = studSpec.Rows[0]["MiddleName"].ToString(),
-            Score = Convert.ToInt32(studSpec.Rows[0]["Score"].ToString()),
-            Priorities = new List<PriorityDto>()
-            {
-                new PriorityDto()
-                {
-                    Order = Convert.ToInt32(studSpec.Rows[0]["Orderr"].ToString()),
-                    SpecialityId = Convert.ToInt32(studSpec.Rows[0]["SpecialtyID"].ToString())
-                },
-                new PriorityDto()
-                {
-                    Order = Convert.ToInt32(studSpec.Rows[1]["Orderr"].ToString()),
-                    SpecialityId = Convert.ToInt32(studSpec.Rows[1]["SpecialtyID"].ToString())
-                },
-                new PriorityDto()
-                {
-                    Order = Convert.ToInt32(studSpec.Rows[2]["Orderr"].ToString()),
-                    SpecialityId = Convert.ToInt32(studSpec.Rows[2]["SpecialtyID"].ToString())
-                }
-            }
-        });
-        //foreach(DataRow datR in studSpec.Rows)
-        //{
-        //    new PriorityDto()
-        //    {
-        //        Order = Convert.ToInt32(datR["Order"].ToString()),
-        //        SpecialityId = Convert.ToInt32(datR["SpecialtyID"].ToString())
-        //    };
-        //}
-    }
-    DataTable specDT = sp.SpecialtySelect();
-    fakeSpecialities = new List<SpecialityDto>(){};
-    foreach(DataRow dr in specDT.Rows)
-    {
-        fakeSpecialities.Add(
-            new SpecialityDto()
-            {
-                Id = Convert.ToInt32(dr["ID"].ToString()),
-                BudgetPlacesCount = Convert.ToInt32(dr["Count"].ToString()),
-                Name = dr["Name"].ToString()
-            });
-    }
-    }
+            return fakeStudents.Where(
+              s => (string.IsNullOrWhiteSpace(filter.Surname) || s.Surname.ToLower().StartsWith(filter.Surname.ToLower())) &&
+                   (!filter.SpecialityId.HasValue || s.Priorities.Any(p => p.SpecialityId == filter.SpecialityId)));
 
-    private List<StudentDto> fakeStudents;
-    private List<SpecialityDto> fakeSpecialities;
-  }
+        }
+
+        public IEnumerable<SpecialityDto> GetAllSpecialities()
+        {
+            return fakeSpecialities;
+        }
+
+        public FakeDataService()
+        {
+            fakeStudents = new List<StudentDto>() { };
+            DataTable studDT = std.StudentSelect(1);
+            int ID = 0, rowIndex = 0;
+            foreach (DataRow dr in studDT.Rows)
+            {
+                if (ID != Convert.ToInt32(dr["ID"].ToString()))
+                {
+                    ID = Convert.ToInt32(dr["ID"].ToString());
+                    rowIndex = studDT.Rows.IndexOf(dr);
+                    fakeStudents.Add(new StudentDto()
+                    {
+                        Surname = dr["Surname"].ToString(),
+                        FirstName = dr["FirstName"].ToString(),
+                        MiddleName = dr["MiddleName"].ToString(),
+                        Score = Convert.ToInt32(dr["Score"].ToString()),
+                        Priorities = new List<PriorityDto>()
+                {
+                    new PriorityDto()
+                    {
+                        Order = Convert.ToInt32(studDT.Rows[rowIndex]["Orderr"].ToString()),
+                        SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex]["SpecID"].ToString())
+                    },
+                    new PriorityDto()
+                    {
+                        Order = Convert.ToInt32(studDT.Rows[rowIndex+1]["Orderr"].ToString()),
+                        SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex+1]["SpecID"].ToString())
+                    },
+                    new PriorityDto()
+                    {
+                        Order = Convert.ToInt32(studDT.Rows[rowIndex+2]["Orderr"].ToString()),
+                        SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex+2]["SpecID"].ToString())
+                    }
+                }
+                    });
+                }
+                //foreach(DataRow datR in studSpec.Rows)
+                //{
+                //    new PriorityDto()
+                //    {
+                //        Order = Convert.ToInt32(datR["Order"].ToString()),
+                //        SpecialityId = Convert.ToInt32(datR["SpecialtyID"].ToString())
+                //    };
+                //}
+            }
+            DataTable specDT = spec.SpecialtySelect();
+            fakeSpecialities = new List<SpecialityDto>() { };
+            foreach (DataRow dr in specDT.Rows)
+            {
+                fakeSpecialities.Add(
+                    new SpecialityDto()
+                    {
+                        Id = Convert.ToInt32(dr["specID"].ToString()),
+                        BudgetPlacesCount = Convert.ToInt32(dr["specCount"].ToString()),
+                        Name = dr["specName"].ToString()
+                    });
+            }
+        }
+
+        private List<StudentDto> fakeStudents;
+        private List<SpecialityDto> fakeSpecialities;
+    }
 }
