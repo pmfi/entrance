002840e2d95a814e5368ef2e69e32441133c35af
NikitaOMGTU55 <skipper55rus@mail.ru>
2017-06-10 12:24:40 +0600

    final version

--- Enrollment/Infrastructure.WebApi/Dto/StudentFilterDto.cs
+++ Enrollment/Infrastructure.WebApi/Dto/StudentFilterDto.cs
@@ -1,8 +1,8 @@
 ﻿namespace Infrastructure.WebApi.Dto
 {
-  public class StudentFilterDto
-  {
-    public string Surname { get; set; }
-    public int? SpecialityId { get; set; }
-  }
+    public class StudentFilterDto
+    {
+        public string Surname { get; set; }
+        public int SpecialityId { get; set; }
+    }
 }
