b1d1b6297c77c440d72e84eaf087a985519419cd
NikitaOMGTU55 <skipper55rus@mail.ru>
2017-06-10 12:22:58 +0600

    final version

--- Enrollment/Infrastructure.WebApi/FakeReadingServices/Impl/FakeDataService.cs
+++ Enrollment/Infrastructure.WebApi/FakeReadingServices/Impl/FakeDataService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Data;
 using Infrastructure.WebApi.Dto;
 using Infrastructure.Orm.Entities;
@@ -13,28 +12,14 @@ namespace Infrastructure.WebApi.FakeReadingServices.Impl
         Specialty spec = new Specialty();
         public IEnumerable<StudentDto> GetStudentsByFilter(StudentFilterDto filter)
         {
-            return fakeStudents.Where(
-              s => (string.IsNullOrWhiteSpace(filter.Surname) || s.Surname.ToLower().StartsWith(filter.Surname.ToLower()) || s.Surname.Contains(filter.Surname)) &&
-                   (!filter.SpecialityId.HasValue || s.Priorities.Any(p => p.SpecialityId == filter.SpecialityId)));
-
-        }
-
-        public IEnumerable<SpecialityDto> GetAllSpecialities()
-        {
-            return fakeSpecialities;
-        }
-
-        public FakeDataService()
-        {
             fakeStudents = new List<StudentDto>() { };
-            DataTable studDT = std.StudentSelect(1);
-            int ID = 0, rowIndex = 0;
+            DataTable studDT = std.StudentSelect(filter.Surname,filter.SpecialityId,1);
+            int ID = 0;
             foreach (DataRow dr in studDT.Rows)
             {
                 if (ID != Convert.ToInt32(dr["ID"].ToString()))
                 {
                     ID = Convert.ToInt32(dr["ID"].ToString());
-                    rowIndex = studDT.Rows.IndexOf(dr);
                     fakeStudents.Add(new StudentDto()
                     {
                         Surname = dr["Surname"].ToString(),
@@ -45,31 +30,22 @@ namespace Infrastructure.WebApi.FakeReadingServices.Impl
                 {
                     new PriorityDto()
                     {
-                        Order = Convert.ToInt32(studDT.Rows[rowIndex]["Orderr"].ToString()),
-                        SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex]["SpecID"].ToString())
-                    },
-                    new PriorityDto()
-                    {
-                        Order = Convert.ToInt32(studDT.Rows[rowIndex+1]["Orderr"].ToString()),
-                        SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex+1]["SpecID"].ToString())
-                    },
-                    new PriorityDto()
-                    {
-                        Order = Convert.ToInt32(studDT.Rows[rowIndex+2]["Orderr"].ToString()),
-                        SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex+2]["SpecID"].ToString())
+                        Order = Convert.ToInt32(dr["Orderr"].ToString()),
+                        SpecialityId = Convert.ToInt32(dr["SpecID"].ToString())
                     }
                 }
                     });
                 }
-                //foreach(DataRow datR in studSpec.Rows)
-                //{
-                //    new PriorityDto()
-                //    {
-                //        Order = Convert.ToInt32(datR["Order"].ToString()),
-                //        SpecialityId = Convert.ToInt32(datR["SpecialtyID"].ToString())
-                //    };
-                //}
             }
+            return fakeStudents;
+            //  fakeStudents.Where(
+            //s => (string.IsNullOrWhiteSpace(filter.Surname) || s.Surname.ToLower().StartsWith(filter.Surname.ToLower()) || s.Surname.Contains(filter.Surname)) &&
+            //     (!filter.SpecialityId.HasValue || s.Priorities.Any(p => p.SpecialityId == filter.SpecialityId)));
+
+        }
+
+        public IEnumerable<SpecialityDto> GetAllSpecialities()
+        {
             DataTable specDT = spec.SpecialtySelect();
             fakeSpecialities = new List<SpecialityDto>() { };
             foreach (DataRow dr in specDT.Rows)
@@ -82,7 +58,49 @@ namespace Infrastructure.WebApi.FakeReadingServices.Impl
                         Name = dr["specName"].ToString()
                     });
             }
+            return fakeSpecialities;
         }
+
+        //public FakeDataService()
+        //{
+        //    fakeStudents = new List<StudentDto>() { };
+        //    DataTable studDT = std.StudentSelect(1);
+        //    int ID = 0, rowIndex = 0;
+        //    foreach (DataRow dr in studDT.Rows)
+        //    {
+        //        if (ID != Convert.ToInt32(dr["ID"].ToString()))
+        //        {
+        //            ID = Convert.ToInt32(dr["ID"].ToString());
+        //            rowIndex = studDT.Rows.IndexOf(dr);
+        //            fakeStudents.Add(new StudentDto()
+        //            {
+        //                Surname = dr["Surname"].ToString(),
+        //                FirstName = dr["FirstName"].ToString(),
+        //                MiddleName = dr["MiddleName"].ToString(),
+        //                Score = Convert.ToInt32(dr["Score"].ToString()),
+        //                Priorities = new List<PriorityDto>()
+        //        {
+        //            new PriorityDto()
+        //            {
+        //                Order = Convert.ToInt32(studDT.Rows[rowIndex]["Orderr"].ToString()),
+        //                SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex]["SpecID"].ToString())
+        //            },
+        //            new PriorityDto()
+        //            {
+        //                Order = Convert.ToInt32(studDT.Rows[rowIndex+1]["Orderr"].ToString()),
+        //                SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex+1]["SpecID"].ToString())
+        //            },
+        //            new PriorityDto()
+        //            {
+        //                Order = Convert.ToInt32(studDT.Rows[rowIndex+2]["Orderr"].ToString()),
+        //                SpecialityId = Convert.ToInt32(studDT.Rows[rowIndex+2]["SpecID"].ToString())
+        //            }
+        //        }
+        //            });
+        //        }
+        //    }
+            
+        //}
 
         private List<StudentDto> fakeStudents;
         private List<SpecialityDto> fakeSpecialities;
